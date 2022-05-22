0830f6c178c104d38ce1fc85c88a47e7e5e87ef9
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-05-22 14:48:42 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j<=i)             {             printf("%d",j);             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c=65;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
         {
-            printf("%c ",c);
+            if(j<=i)
+            {
+            printf("%d",j);
+            }
         }
-        c++;
         printf("
 ");
     }
