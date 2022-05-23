4e5e73fcc507ca41bf0f1dbea55713d91e026ed7
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-05-23 06:16:35 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='A' &&str[i]<='Z')         {             c=c+1;         }     }     printf("%d",c); }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='A' &&str[i]<='Z')
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
