46e6e5493ae85c949ded6f100dce7262a2a78b04
vishnupriyakovvuri <97426019+vishnupriyakovvuri@users.noreply.github.com>
2022-05-29 11:45:59 +0530

    #include<stdio.h> int main() {     int n,i,a[100],b[100],j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     j=0;     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             b[j++]=a[i];         }     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             b[j++]=a[i];         }     }     for(j=0;j<n;j++)     {         printf("%d ",b[j]);     }
    }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],b[100],j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    j=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            b[j++]=a[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            b[j++]=a[i];
+        }
+    }
+    for(j=0;j<n;j++)
+    {
+        printf("%d ",b[j]);
+    }
+
+}
